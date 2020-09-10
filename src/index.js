bda5c0abf83c26a4932b3d8865290d2b7529205c
Aline <alinesanqueza@gmail.com>
2020-09-10 11:14:03 -0300

    css e js sem a lógica

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// import "./style.css";
+
+//Manipulação do dom
+const video = document.querySelector("#video");
+const canvas = document.querySelector("#canvas");
+const snap = document.querySelector("#snap");
+
+//Renderizar imagem da webcam no navegador
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    handleSuccess(stream);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Função para sucesso
+const handleSuccess = (stream) => {
+  window.stream = stream;
+  video.srcObject = stream;
+};
+
+// Função para desenhar imagem na tela
+const toDrawCapturedSnap = () => {
+  var context = canvas.getContext("2d");
+  //desenhar canvas (x,y,width, height)
+  context.drawImage(video, 10, 10, canvas.width, canvas.height);
+  //testar se deu certo fazendo download da imagem com botao direito
+  //formato default: png
+
+  //Transformar canva em imagem jpeg com qualidade 0.7
+  const dataURI = canvas.toDataURL("image/jpeg", 0.7);
+  //como obter uma url da imagem armazenada em dataURI?
+  //salvar no firebase?
+};
+
+//Atribuir captura com o click
+snap.addEventListener("click", toDrawCapturedSnap);
+
+//Iniciar com o carregamento da página!
+window.onload = init();
